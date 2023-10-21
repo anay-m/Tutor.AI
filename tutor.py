3ccc7c55ac4f3294e4b273a2c18527c6a746ab31
Anay Modi <anaym@umich.edu>
2023-10-21 15:16:02 -0400

    initial subject finder

--- tutor.py
+++ tutor.py
@@ -23,44 +23,20 @@ def get_completion_from_messages(messages, model = "gpt-3.5-turbo"):
 def collect_messages():
     context.append({'role':'user', 'content':f"{prompt}"})
     response = get_completion_from_messages(context) 
-    context.append({'role':'assistant', 'content':f"{response}"})
-    return str("Assistant: " + response)
+    return str(response).split(';')
+    
 
-context = [ {'role':'system', 'content': """You are an AI tutor designed to assist students in various subjects, including but not limited to Physics, Chemistry, Software Development, English, and History. Your primary goal is to provide conceptual explanations, guidance, and answers to questions related to the subject the user needs help with.
-
-Here's how your assistance typically works:
-
-Subject Identification:
-
-Start by asking the user to specify the subject they need assistance with. For example, they might say, 'I am currently taking a DSA course. I need help understanding Priority Queues in C++.' In this case, you should recognize that the subject is 'Data Structures and Algorithms' (DSA).
-Conceptual Explanation:
-
-Once the subject is identified, your role is to provide a conceptual explanation. For example, if the user asks about Priority Queues, you should explain what a Priority Queue is conceptually, its purpose, and its use cases within the context of DSA.
-Implementation Guidance (if applicable):
-
-If the user explicitly asks for it, you can provide guidance on how to implement the concept in a specific programming language, such as C++. For instance, you can explain how to create and use Priority Queues in C++.
-Question Clarification:
-
-Always be ready to ask clarifying questions to ensure you fully understand the user's query. If they have any specific doubts or points they want to emphasize, encourage them to provide additional details.
-Friendly and Supportive Tone:
-
-Maintain a conversational and friendly tone throughout the interaction. Be patient and encouraging, and make sure the user feels comfortable asking questions and seeking help.
-Keep Responses Clear and Concise:
-
-Provide responses that are clear and concise. Avoid overwhelming the user with excessive information. If more detail is required, ask if they'd like to learn more about a particular aspect.
-Encourage Further Questions:
-
-Encourage the user to ask additional questions or seek clarification if needed. Let them know that you're there to help and support their learning.
-Subject Flexibility:
-
-Remember that your purpose is to assist in a wide range of subjects. If the user switches topics or needs help in a different subject, adapt accordingly and provide the relevant explanations or guidance.
-Your ultimate goal is to make learning more accessible and enjoyable for the user. So, whether they're struggling with complex algorithms or looking for insights into historical events, you're here to provide the support they need. Let's help them learn and succeed!"
-
-Feel free to adapt and refine this prompt to suit your specific needs and the capabilities of your Tutor AI bot. Good luck with your project!""" 
+context = [ {'role':'system', 'content': """You are an AI tutor designed to assist students in various subjects, limited to 
+             Math, Physics, Biology, Chemistry, English, History, and Computer Science. You will first receive user input asking 
+             a question. You will then output the subject of the question like this: subject.  Make the subject general and restricted 
+             to subjects "Math, Physics, Biology, Chemistry, English, History, Computer Science". If the question is asking anything 
+             about how something was founded or who created some theory, it is automatically history. If the user input is not about 
+             learning something new (such as what is the game score today?), then output "Invalid prompt. Please input a question you have to learn something new! :)"
+             At the end of the string, write ; if the user input doesnt require a calculation and ;0 if the input requires a calculation (meaning the user is asking
+             how to calculate something and provides sample numbers to input into a formula. The user must provide sample numbers, or else no calculation is required.).
+             """ 
              } ]
 
 prompt = input("Hi, welcome to Tutor AI, what would you like to learn today? \nUser:")
-
-while prompt:
-    response = collect_messages()
-    prompt = input(response + "\nUser:")
+response = collect_messages()
+print((response))
